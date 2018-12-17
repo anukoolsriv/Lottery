8c9bb6dccb7232fa84168fae26f7ec23f05bfd43
Anukool Srivastava <anukoolsriv@gmail.com>
2018-12-17 21:37:42 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -5,6 +5,9 @@ const solc = require('solc');
 
 const lotteryPath = path.resolve(__dirname,'contracts','Lottery.sol'); //__dirname constant set to cwd
 const source = fs.readFileSync(lotteryPath,'utf8');	// contains .sol code
+// "source" variable is fetching the solidity code successfully as defined in the path location
 
 //module.exports = solc.compile(source,1).contracts[':Lottery'];	// no of contracts trying to compile
  console.log(solc.compile(source,1));
+
+//Unable to understand why solc.compile is returning "undefined" in the above code.
