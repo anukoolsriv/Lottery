3e57fafb935147ede40d906574dfa00c029f1d18
Anukool Srivastava <anukoolsriv@gmail.com>
2018-12-17 21:46:10 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -7,9 +7,9 @@ const lotteryPath = path.resolve(__dirname,'contracts','Lottery.sol'); //__dirna
 const source = fs.readFileSync(lotteryPath,'utf8');	// contains .sol code
 // "source" variable is fetching the solidity code successfully as defined in the path location
 
-//module.exports = solc.compile(source,1).contracts[':Lottery'];	// no of contracts trying to compile
- console.log(solc.compile(source,1));
+module.exports = solc.compile(source,1).contracts[':Lottery'];	// no of contracts trying to compile
 
-// Unable to understand why solc.compile is returning "undefined" in the above code.
+
+// Unable to understand why "solc.compile()" is returning "undefined" in the above code.
 // I have another project with similar code to test using mocha. That project is running successfully during testing
 //Im confused why this one isnt.
